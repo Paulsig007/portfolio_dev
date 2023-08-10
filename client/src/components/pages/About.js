3e14ee910664d8a4c77eb37f554dd4d3990a8c22
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-08-10 15:46:49 -0600

    update resume, update bio in about.js component

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,12 +7,25 @@ export default function About() {
       <div className="textContainer">
         <h2 className="compTitle aboutTitle">It's a pleasure to meet you!</h2>
         <p className="aboutText">
-          I am a full stack web developer with a passion for learning and
-          creating. I have always been fascinated by technology and how it can
-          be used to solve problems. In the short time I have spent in
-          developement, I can honestly say that I love what I do and I am
-          excited to continue to learn as a developer, grow as a person, engage
-          with a dynamic team, and contribute to the tech community.
+          Greetings! I'm not just a full stack software engineer; I'm an
+          enthusiastic explorer of the boundless realm where learning and
+          creation intertwine. From the very roots of my fascination with
+          technology's potential to unravel challenges, I've embarked on a
+          journey that's ignited an insatiable passion for development.
+          <br></br>
+          <br></br>
+          In this condensed span of time immersed in the art of crafting digital
+          landscapes, I've unequivocally found my calling. Enthusiasm courses
+          through my veins as I eagerly embrace the endless avenues of growth as
+          a developer and as an individual. A fervent believer in the power of
+          collaboration, I'm on the hunt for a dynamic team that mirrors my
+          ardor for innovation.
+          <br></br>
+          <br></br>
+          But my aspirations don't halt there; my sights are set on weaving my
+          narrative into the vibrant tapestry of the tech community. So, let's
+          dive into tomorrow's solutions together, as I strive to make every
+          line of code a testament to my unwavering commitment to progress.
         </p>
         {/* <br></br> */}
         <h2 className="skillTitle">My Skills</h2>
@@ -23,7 +36,7 @@ export default function About() {
           Mongoose - IDB - PWAs - MVC Paradigm - Agile Developement - SQ - MySQL
           Workbench - Sequelize (Currently learning: React Native, Redux, and )
         </p>
-        <a href={resume} download>
+        <a id="resume" href={resume} download>
           Download My Resume (PDF)
         </a>
       </div>
