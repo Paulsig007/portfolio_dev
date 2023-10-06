c40219d4c922ac6d6fd3b47e4f91588ebdf6685e
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-10-06 10:12:11 -1000

    add language for ma peepl and Social Network API to include that there is no depoloyed APP. Add conditional rendering style to View Depolyed App button

--- client/src/assets/projectObjects.js
+++ client/src/assets/projectObjects.js
@@ -6,48 +6,48 @@ import Chat98 from "./images/chat98.png";
 import MoraConsulting from "./images/MoraConsulting.png";
 
 const projectObjects = [
-    {
-      name: "Mora Consulting",
-      cardImg: MoraConsulting,
-      repoUrl: "https://github.com/Paulsig007/shayna-mora-consulting",
-      deployedUrl: "https://moraconsulting.co/",
-      descriptions: "My first freelance project",
-    },
-    {
-      name: "Chat98",
-      cardImg: Chat98,
-      repoUrl: "https://github.com/TrueWillB/chat98",
-      deployedUrl: "https://chat98-a21833346cf4.herokuapp.com/",
-      descriptions: "A nostalgic chatroom app",
-    },
-    {
-      name: "Blueit",
-      cardImg: Blueit,
-      repoUrl: "https://github.com/Paulsig007/Blueit",
-      deployedUrl: "https://blueit-79b9e15820d2.herokuapp.com/",
-      descriptions: "A simple blog posting app",
-    },
-    {
-      name: "Social Network API",
-      cardImg: SocialNetworkAPI,
-      repoUrl: "https://github.com/Paulsig007/mySocialAPI",
-      deployedUrl: "N/A",
-      descriptions: "It basically explains itself",
-    },
-    {
-      name: "MaPeepl",
-      cardImg: MaPeepl,
-      repoUrl: "https://github.com/Paulsig007/MaPeepl",
-      deployedUrl: "N/A",
-      descriptions: "A CLI Employee Tracker",
-    },
-    {
-      name: "Dr. Drink",
-      cardImg: DrDrink,
-      repoUrl: "https://github.com/ryantixier/Dr-Drink",
-      deployedUrl: "https://ryantixier.github.io/Dr-Drink/",
-      descriptions: "A delightful cocktail recipe app",
-    },
-  ];
+  {
+    name: "Mora Consulting",
+    cardImg: MoraConsulting,
+    repoUrl: "https://github.com/Paulsig007/shayna-mora-consulting",
+    deployedUrl: "https://moraconsulting.co/",
+    descriptions: "My first freelance project",
+  },
+  {
+    name: "Chat98",
+    cardImg: Chat98,
+    repoUrl: "https://github.com/TrueWillB/chat98",
+    deployedUrl: "https://chat98-a21833346cf4.herokuapp.com/",
+    descriptions: "A nostalgic chatroom app",
+  },
+  {
+    name: "Blueit",
+    cardImg: Blueit,
+    repoUrl: "https://github.com/Paulsig007/Blueit",
+    deployedUrl: "https://blueit-79b9e15820d2.herokuapp.com/",
+    descriptions: "A simple blog posting app",
+  },
+  {
+    name: "Social Network API",
+    cardImg: SocialNetworkAPI,
+    repoUrl: "https://github.com/Paulsig007/mySocialAPI",
+    deployedUrl: "#",
+    descriptions: "A Fun little API (No Deployed URL)",
+  },
+  {
+    name: "MaPeepl",
+    cardImg: MaPeepl,
+    repoUrl: "https://github.com/Paulsig007/MaPeepl",
+    deployedUrl: "#",
+    descriptions: "CLI Employee Tracker (No Deployed URL)",
+  },
+  {
+    name: "Dr. Drink",
+    cardImg: DrDrink,
+    repoUrl: "https://github.com/ryantixier/Dr-Drink",
+    deployedUrl: "https://ryantixier.github.io/Dr-Drink/",
+    descriptions: "A delightful cocktail recipe app",
+  },
+];
 
-    export default projectObjects;
+export default projectObjects;
--- client/src/pages/projects/Projects.js
+++ client/src/pages/projects/Projects.js
@@ -9,53 +9,63 @@ import {
 import projectObjects from "../../assets/projectObjects";
 import styles from "./Projects.module.css";
 
-
 export default function Projects() {
   return (
     <div className={styles.componentContainer}>
       <div className={styles.projectBackground}>
-      <h1 className={styles.projectTitle}>Projects</h1>
-      <p className={styles.projectText}>
-        Please feel free to browse through my projects. I have included links to
-        the repositories as well as the deployed applications.
-      </p>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "20px",
-          justifyContent: "center",
-          marginBottom: "4%"
-        }}
-      >
-        {projectObjects.map((project, index) => (
-          <Card
-            key={index}
-            className=".projectCard"
-            style={{ minWidth: 300, cursor: "pointer" }}
-          >
-            <CardMedia
-              component="img"
-              height="130"
-              image={project.cardImg}
-              alt={project.name}
-              onClick={ () => (window.location.href = `${project.deployedUrl}`)}
-            />
-            <CardContent>
-              <Typography variant="h6" component="div">
-                {project.name}
-              </Typography>
-              <Typography variant="body1">{project.descriptions}</Typography>
-              <Button href={project.repoUrl} target="_blank">
-                View Repo
-              </Button>
-              <Button href={project.deployedUrl} target="_blank">
-                View Deployed
-              </Button>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+        <h1 className={styles.projectTitle}>Projects</h1>
+        <p className={styles.projectText}>
+          Please feel free to browse through my projects. I have included links
+          to the repositories as well as the deployed applications.
+        </p>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "20px",
+            justifyContent: "center",
+            marginBottom: "4%",
+          }}
+        >
+          {projectObjects.map((project, index) => (
+            <Card
+              key={index}
+              className=".projectCard"
+              style={{ minWidth: 340, cursor: "pointer" }}
+            >
+              <CardMedia
+                component="img"
+                height="130"
+                image={project.cardImg}
+                alt={project.name}
+                onClick={() =>
+                  (window.location.href = `${project.deployedUrl}`)
+                }
+              />
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {project.name}
+                </Typography>
+                <Typography variant="body1">{project.descriptions}</Typography>
+                <Button href={project.repoUrl} target="_blank">
+                  View Repo
+                </Button>
+                {/* if deployedURL=== # do not render */}
+                <Button
+                  href={project.deployedUrl}
+                  target="_blank"
+                  style={
+                    project.deployedUrl === "#"
+                      ? { display: "none" }
+                      : { display: "inline" }
+                  }
+                >
+                  View Deployed App
+                </Button>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
